6fe6b5b6e51877efcca2d3fe45be98ec4a27db81
francocortina <franco.cug@gmail.com>
2022-02-21 20:37:41 -0300

    mostrando display

---
+++ Ejercicio10functions/script.js
@@ -0,0 +1,137 @@
+
+//ARRAY Y LISTAS
+
+  let item = "flash";
+  const collection =["Franco",item, 5, true];
+
+//Elimina el último elemento
+collection.pop();
+//Agrega al principio
+collection.unshift("virtual")
+//agrega el del final al principio
+collection.unshift(collection.pop());
+  //agregar
+collection.push("red");
+
+
+//hallar un item especifico
+const findItem= collection.find(item => item==="cat");
+console.log("found item", findItem);
+
+
+//splice remlaza , agrega o elimina
+//(posicion,cantidad de eliminados luego de la posicion, valor agregado opcional)
+  collection.splice(1,2,"brown");
+   //este es especifico para eliminar algo
+  collection.splice(collection.indexOf("brown"),1,"black")
+
+  //ordenarlos alfabeticamente por default
+ collection.sort();
+  console.log(collection);
+
+  
+  collection.forEach(function(item){
+   item= `<li> ${item}</li>`
+  
+    
+   
+    
+    console.log(item);
+   
+
+  })
+
+
+  const Objt={
+    name:"franco",
+    apellido:"Cortina"
+  }
+  const arrayfc = [
+"Franco",5,Objt
+  ];
+  console.log(Objt);
+  console.log(arrayfc);
+
+  arrayfc.forEach(function(item){
+    item =`<li> ${item}</li>`
+    console.log(item);
+  })
+ 
+
+function sumar (a,b){
+
+  let suma=a+b;
+  console.log(suma);
+
+}
+
+sumar(24,25);
+
+
+var resta=25;
+console.log(resta);
+function restar (a,b){
+resta= a-b;
+return resta;
+}
+restar(25,36);
+console.log(resta)
+console.log(restar(300,125));
+
+const arrayprueba=["Franco","Hernesto","Manuel", "Micaela"];
+console.log(arrayprueba);
+
+
+arrayprueba.forEach(function(item){
+
+  item=item;
+  console.log(item);
+})
+
+for (let index = 0; index < arrayprueba.length; index++) {
+  const element = arrayprueba[index];
+  console.log(element);
+  
+}
+
+
+Object2 ={
+name:"franco",
+age:24,
+email:"franco.cug@gmail.com",
+universidad:"UTN-FRLP"
+
+};
+
+console.log("MAIL:",Object2.email)
+
+//creo el Objeto
+const Person={
+  name:"Franco",
+  apellido:"Cortina Ugolini",
+  email:"franco.cug@gmail.com",
+  edad:24,
+
+
+};
+
+//creo la funcion que pase un objeto y lo descomponga en cuanto al objeto
+const addPerson= function (currentPerson) {
+  const newArticle = document.createElement("article");
+  newArticle.innerHTML= `
+  <h1>Nombre:${currentPerson.name}</h1>
+<ul>
+<li>${currentPerson.apellido}</li>
+<li>${currentPerson.email}</li>
+<li>${currentPerson.edad}</li>
+</ul>
+  
+  `
+  return newArticle;
+  
+};
+
+//imprimo en el main adosandolo con el append a lo que ya está
+const main = document.querySelector("main");
+//paso el objeto a la función .
+main.append(addPerson(Person));
--- Ejercicio9ArraysAndList 2/script.js
+++ Ejercicio9ArraysAndList 2/script.js
@@ -4,30 +4,65 @@
   let item = "flash";
   const collection =["Franco",item, 5, true];
 
-console.log(collection);
-console.log(collection[2]);
-//ESTRUCTURA DE FOR
-for (let index = 0; index < collection.length; index++) {
-  const element = collection[index];
-  console.log(element);
+//Elimina el último elemento
+collection.pop();
+//Agrega al principio
+collection.unshift("virtual")
+//agrega el del final al principio
+collection.unshift(collection.pop());
+  //agregar
+collection.push("red");
+
+
+//hallar un item especifico
+const findItem= collection.find(item => item==="cat");
+console.log("found item", findItem);
+
+
+//splice remlaza , agrega o elimina
+//(posicion,cantidad de eliminados luego de la posicion, valor agregado opcional)
+  collection.splice(1,2,"brown");
+   //este es especifico para eliminar algo
+  collection.splice(collection.indexOf("brown"),1,"black")
+
+  //ordenarlos alfabeticamente por default
+ collection.sort();
+  console.log(collection);
+
+  const main = document.querySelector(".maincontent");
+  collection.forEach(function(item){
+   item= `<li> ${item}</li>`
+  
+    
+   
+    
+    console.log(item);
+   
+
+  })
+
+
+  const Objt={
+    name:"franco",
+    apellido:"Cortina"
+  }
+  const arrayfc = [
+"Franco",5,Objt
+  ];
+  console.log(Objt);
+  console.log(arrayfc);
+
+  arrayfc.forEach(function(item){
+    item =`<li> ${item}</li>`
+    console.log(item);
+  })
+ 
+
+function sumar (a,b){
+
+  let suma=a+b;
+  console.log(suma);
 
 }
 
-// ESTRUCTURA forEach
-collection.forEach(function(item){{
-  item=`<li> ${item}</li>`
-  console.log(item);
-  
-}})
-
-
-//Estrutura find
-
-let longItems= collection.find(function(item){{
-  if(item.length>=5){
-    return item;
-  }
-}})
-console.log("longItems:  ", longItems);
-console.log(collection);
-
+sumar(24,25);
